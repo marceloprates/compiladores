5f369be289cae865b0c3bcdce822b0a46151f901
lucas <lucas@Hive.(none)>
2013-06-15 15:33:26 -0300

    Atualizei os TACs

--- Etapa 5/tac.c
+++ Etapa 5/tac.c
@@ -2,20 +2,20 @@
 
 TAC* tac_join(TAC* tac1, TAC* tac2)
 {
-	if(tac1 == NULL)
-		return tac2;
-
 	if(tac2 == NULL)
+		return tac1;
+
+	if(tac1 == NULL)
 		return NULL;
 
-	TAC* p = tac1;
+	TAC* p = tac2;
 
 	while(p->prev != NULL)
 	{
 		p = p->prev;
 	}
 
-	p->prev = tac2;
+	p->prev = tac1;
 
-	return tac1;
+	return tac2;
 }
--- Etapa 5/tac.h
+++ Etapa 5/tac.h
@@ -2,13 +2,30 @@
 
 enum TAC_type
 {
-	ADD,
-	SUB,
-	MUL,
-	DIV,
-	IF0,
-	LABEL,
-	LD
+	TAC_SYMBOL,
+	TAC_MOVE,
+	TAC_ADD,
+	TAC_SUB,
+	TAC_MUL,
+	TAC_DIV,
+	TAC_LESS,
+	TAC_LESS_EQUAL,
+	TAC_GREATER,
+	TAC_GREATER_THAN,
+	TAC_EQUAL,
+	TAC_NOT_EQUAL,
+	TAC_AND,
+	TAC_OR,
+	TAC_LABEL,
+	TAC_BEGINFUN,
+	TAC_ENDFUN,
+	TAC_IFZ,
+	TAC_JUMP,
+	TAC_CALL,
+	TAC_ARG,
+	TAC_RET,
+	TAC_PRINT,
+	TAC_READ
 };
 
 struct TAC_struct
@@ -22,3 +39,5 @@ struct TAC_struct
 };
 
 typedef struct TAC_struct TAC;
+
+TAC* tac_join(TAC* tac1, TAC* tac2);
